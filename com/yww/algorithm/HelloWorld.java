b0aa899d69a1e130dbc8147152f68d07a14142f3
chenhao <1141950370@qq.com>
2022-08-26 16:27:42 +0800

    初始化工程

---
+++ com/yww/algorithm/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.yww.algorithm;
+
+/**
+ * <p>
+ *      学习编程的第一个程序
+ * </p>
+ *
+ * @ClassName HelloWorld
+ * @Author yww
+ * @Date 2022/8/26 16:24
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
